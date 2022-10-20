ddf10207552de441228f10455f82a1d868366052
FurkanAdemoglu <furkanademoglu23@gmail.com>
2022-10-20 21:44:11 +0300

    project is created and png files added

---
+++ MyWeatherApp/MyWeatherApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyWeatherApp
+//
+//  Created by Furkan Ademoğlu on 20.10.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
